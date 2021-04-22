5941cdb758c68d59b6298ad3bd9361274afc0d0d
MadhushaPrasad <madushaprasad21@gmail.com>
2021-04-22 16:59:08 +0530

    Change file structure and add HomePage.js file with Homepage.css

--- src/components/countryList/CountryList.js
+++ src/components/countryList/CountryList.js
@@ -1,77 +1,72 @@
-import React from "react";
+import React from 'react';
 import './countryList.css';
-import Country from "../country/Country";
-import axios from "axios";
-
+import Country from '../country/Country';
+import axios from 'axios';
 
 class CountryList extends React.Component {
+  componentDidMount() {
+    // All Response
+    axios
+      .get('https://corona.lmao.ninja/v2/countries?sort=country')
+      .then((response) => {
+        this.setState({ all: response.data });
+        this.setState({ country: response.data[31].country });
+        this.setState({ countryInfo: response.data[31].countryInfo });
+        this.setState({ deaths: response.data[31].deaths });
+        this.setState({ recovered: response.data[31].recovered });
+        this.setState({ tests: response.data[31].tests });
+        this.setState({ todayCases: response.data[31].todayCases });
+        this.setState({ todayDeaths: response.data[31].todayDeaths });
+        this.setState({ todayRecovered: response.data[31].todayRecovered });
+        this.setState({ updatedTime: response.data[31].updatedTime });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    componentDidMount() {
-        // All Response
-        axios.get("https://corona.lmao.ninja/v2/countries?sort=country")
-            .then(response => {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      all: [],
+      country: '',
+      countryInfo: [],
+      deaths: 0,
+      recovered: 0,
+      tests: 0,
+      todayCases: 0,
+      todayDeaths: 0,
+      todayRecovered: 0,
+      updatedTime: 45564,
+    };
+  }
 
-                this.setState({all: response.data})
-                this.setState({country: response.data[31].country})
-                this.setState({countryInfo: response.data[31].countryInfo})
-                this.setState({deaths: response.data[31].deaths})
-                this.setState({recovered: response.data[31].recovered})
-                this.setState({tests: response.data[31].tests})
-                this.setState({todayCases: response.data[31].todayCases})
-                this.setState({todayDeaths: response.data[31].todayDeaths})
-                this.setState({todayRecovered: response.data[31].todayRecovered})
-                this.setState({updatedTime: response.data[31].updatedTime})
+  render() {
+    //const da = this.state.all;
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }
+    const render_details = (da, index) => {
+      return (
+        <Country
+          key={index}
+          country={da.country}
+          countryInfo={da.countryInfo}
+          deaths={da.deaths}
+          recovered={da.recovered}
+          tests={da.tests}
+          todayCases={da.todayCases}
+          todayDeaths={da.todayDeaths}
+          todayRecovered={da.todayRecovered}
+          updatedTime={da.updatedTime}
+        />
+      );
+    };
 
-    constructor(prop) {
-        super(prop);
-        this.state = {
-            all: [],
-            country: '',
-            countryInfo: [],
-            deaths: 0,
-            recovered: 0,
-            tests: 0,
-            todayCases: 0,
-            todayDeaths: 0,
-            todayRecovered: 0,
-            updatedTime: 45564
-        }
-    }
-
-    render() {
-
-        const da = this.state.all;
-
-        const render_details = (da, index) => {
-            return (
-                <Country
-                    key={index}
-                    country={da.country}
-                    countryInfo={da.countryInfo}
-                    deaths={da.deaths}
-                    recovered={da.recovered}
-                    tests={da.tests}
-                    todayCases={da.todayCases}
-                    todayDeaths={da.todayDeaths}
-                    todayRecovered={da.todayRecovered}
-                    updatedTime={da.updatedTime}
-                />
-            )
-        };
-
-        return (
-            <div className="countryDivList row d-flex justify-content-around">
-                    {this.state.all.map(render_details)}
-            </div>
-        );
-    }
-
+    return (
+      <div className="countryDivList row bg-white d-flex justify-content-around">
+        {this.state.all.map(render_details)}
+      </div>
+    );
+  }
 }
 
 export default CountryList;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,51 +1,87 @@
-import React from "react";
+import React from 'react';
 import './footer.css';
 
-
 class Footer extends React.Component {
-    render() {
-        return (
-            <footer class="site-footer">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-sm-12 col-md-6">
-                            <h6>About</h6>
-                            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-                        </div>
+  render() {
+    return (
+      <footer class="site-footer">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <h6>About</h6>
+              <p class="text-justify">
+                Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative
+                to help the upcoming programmers with the code. Scanfcode
+                focuses on providing the most efficient code or snippets as the
+                code wants to be simple. We will help programmers build up
+                concepts in different programming languages that include C, C++,
+                Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and
+                Algorithm.
+              </p>
+            </div>
 
-                        <div class="offset-2 col-xs-6 col-md-3">
-                            <h6>Quick Links</h6>
-                            <ul class="footer-links">
-                                <li><a href="http://scanfcode.com/about/">Home</a></li>
-                                <li><a href="http://scanfcode.com/contact/">Symptom</a></li>
-                                <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Graph</a></li>
-                                <li><a href="http://scanfcode.com/privacy-policy/">Sri Lanka</a></li>
-                                <li><a href="http://scanfcode.com/sitemap/">About</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <hr/>
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 col-sm-6 col-xs-12">
-                            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
-                                <a href="#"> Madhusha Prasad</a>.
-                            </p>
-                        </div>
+            <div class="offset-2 col-xs-6 col-md-3">
+              <h6>Quick Links</h6>
+              <ul class="footer-links">
+                <li>
+                  <a href="http://scanfcode.com/about/">Home</a>
+                </li>
+                <li>
+                  <a href="http://scanfcode.com/contact/">Symptom</a>
+                </li>
+                <li>
+                  <a href="http://scanfcode.com/contribute-at-scanfcode/">
+                    Graph
+                  </a>
+                </li>
+                <li>
+                  <a href="http://scanfcode.com/privacy-policy/">Sri Lanka</a>
+                </li>
+                <li>
+                  <a href="http://scanfcode.com/sitemap/">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <hr />
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+              <p class="copyright-text">
+                Copyright &copy; 2020 All Rights Reserved by
+                <a href="https://madhushaprasad.s3.ap-south-1.amazonaws.com/index.html">
+                  {' '}
+                  Madhusha Prasad
+                </a>
+                .
+              </p>
+            </div>
 
-                        <div class="col-md-4 col-sm-6 col-xs-12">
-                            <ul class="social-icons">
-                                <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                                <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        )
-    }
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <ul class="social-icons">
+                <li>
+                  <a
+                    class="facebook"
+                    href="https://www.facebook.com/madusha.prasad.397/"
+                  >
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="linkedin"
+                    href="https://www.linkedin.com/in/madhusha-prasad-045a82187"
+                  >
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 export default Footer;
